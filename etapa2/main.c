6c7197c61ba658dda9fd3134c83a3bab78d997ad
arthur fuchs <alfuchs17@gmail.com>
2019-04-08 21:08:58 -0300

    retirada a função readFile do arquivo main.c

--- etapa2/main.c
+++ etapa2/main.c
@@ -13,27 +13,24 @@
 void initMe();
 extern FILE *yyin;
 
-void readFile(char *filename){
-	FILE* file = fopen(filename, "r");
-	if(file == NULL){
-		fprintf(stderr, "%s", "Can't open file. \n");
-		exit(1);
-	}
-
-	yyin = file;
-	yyparse();
-
-	printTable();
-}
-
 int main(int argc, char *argv[]){
   if(argc < 2){
-		fprintf(stderr, "%s", "Missing file name! Command format: ./etapa2 <file_name> \n");
-		exit(1);
-	}
+    fprintf(stderr, "%s", "Missing file name! Command format: ./etapa2 <file_name> \n");
+    exit(1);
+  }
 
   initMe();
-  readFile(argv[1]);
+
+  FILE* file = fopen(argv[1], "r");
+  if(file == NULL){
+    fprintf(stderr, "%s", "Can't open file. \n");
+    exit(1);
+  }
+
+  yyin = file;
+  yyparse();
+
+  printTable();
 
   return 0;
 }
