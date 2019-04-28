237b96d088f739ce11f5639f3a6b17e5b14e0e4c
arthur fuchs <alfuchs17@gmail.com>
2019-04-28 17:50:50 -0300

    implementados os métodos de impressão da ast

--- etapa3/ast.h
+++ etapa3/ast.h
@@ -1,6 +1,8 @@
 #ifndef SYNTAXTREE_H
 #define SYNTAXTREE_H
 
+#include <stdlib.h>
+#include <stdio.h>
 #include "hash.h"
 
 typedef struct syntax_node{
@@ -13,7 +15,7 @@ typedef struct syntax_node{
 } AST;
 
 AST* newSyntaxNode (int type, NODE* symbol, AST* son1, AST* son2, AST* son3, AST* son4);
-void printNode (AST* node);
-void printTree (AST* root);
+void printNode (AST* node, int depth);
+void printTree (AST* root, int depth);
 
 #endif
